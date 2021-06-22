1bcd60657871bf87bf5112986f6e07c9b5b06015
aimwps <mwpskinner@gmail.com>
2021-06-22 18:10:56 +0100

    changed image reshaping for CNN

--- helper_functions.py
+++ helper_functions.py
@@ -3,7 +3,7 @@ def calc_accuracy(mdl, test_data):
     # resulting in most likely label
     total_acc = []
     for images, labels in iter(test_data):
-        images.resize_(images.size()[0],784)
+        #images.resize_(images.size()[0],784)
         max_vals, max_indices = mdl(images).max(1)
         # assumes the first dimension is batch size
         n = max_indices.size(0)  # index 0 for extracting the # of elements
