0c54b346bdbaeb0e1798632d5cc3ea5c7d34148e
aimwps <mwpskinner@gmail.com>
2021-06-22 18:12:02 +0100

    added comments for changing between different NN's

--- sign_language_classifier.py
+++ sign_language_classifier.py
@@ -9,8 +9,6 @@ import torch
 from torchvision import datasets, transforms
 from PIL import Image
 from scipy import stats
-train_df = pd.read_csv("data/train data/sign_mnist_train.csv")
-
 
 class handDetector():
 	def __init__(self, mode=False, maxHands=2, detectionCon = 0.6, trackCon = 0.6):
@@ -65,21 +63,19 @@ class handDetector():
 
 
 		return self.lmList, bbox, hands_region
+
 ################################################################################
 ### For finding and processing the hand
-
 	def process_hand_img(self,img_arr):
 		pil_img = Image.fromarray(img_arr)
-
 		test_transforms = transforms.Compose([
 									transforms.Grayscale(),
 									transforms.Resize((28,28)),
                                     transforms.ToTensor(),
 									transforms.Normalize((0.5,), (0.5,)),
                                       ])
-
 		image_tensor = test_transforms(pil_img).float()
-		image_tensor = torch.flatten(image_tensor).reshape((1,784))
+		image_tensor = torch.flatten(image_tensor).reshape((1,1,28,28))
 		return image_tensor
 
 
@@ -88,9 +84,10 @@ class handDetector():
 		cTime = 0
 		cap = cv2.VideoCapture(0)
 		detector = handDetector()
-		model = torch.load("oldmodels/savedmodel.pth")
+		model = torch.load("nn2_adam_lr001_ep10.pth")
 		current_pred = None
 		avg_pred_tracker = []
+
 		while True:
 			ret, frame = cap.read()
 ################################################################################
@@ -98,22 +95,28 @@ class handDetector():
 			hands_frame = detector.findHands(frame)
 			lmList, bbox, hands_only = detector.findPosition(hands_frame)
 			hands_processed = detector.process_hand_img(hands_only)
+			#print(type(hands_processed))
+
 ################################################################################
 ### For predicting the image of the hand
 			model.eval()
 			with torch.no_grad():
 				max_vals, max_indices = model(hands_processed).max(1)
-				avg_pred_tracker.append(max_indices)
+				print(max_indices[0])
+				avg_pred_tracker.append(max_indices[0].item())
 				if len(avg_pred_tracker)==30:
 					prediction = stats.mode(avg_pred_tracker)[0]
 					current_pred =  prediction
+					print(avg_pred_tracker)
 					avg_pred_tracker = []
 			model.train()
+
 ################################################################################
 ### For outputting to the screen
 			cv2.putText(frame, str(current_pred), (10,70),cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
 			cv2.imshow('hands_frame', frame)
 			cv2.imshow('hands_only', hands_only)
+			#cv2.imshow('processed_hands', hands_processed.numpy())
 
 ################################################################################
 ### For breaking out of video loop use 'q' or 'Q'
