291290dc33b829f30d07ca8caae837ef3756f41d
DanielBiman <daniel.biman@mail.de>
2021-06-23 14:01:35 +0200

    Adding dictionary for label-alphabet
    this helper function translates the prediction from a number to a letter

--- helper_functions.py
+++ helper_functions.py
@@ -25,3 +25,14 @@ def save_sign_img(sign_label, img):
         new_file_name = sign_label + "/" + sign_code + now_code + f"_{str(duplicate_counter)}" + ".jpg"
         duplicate_counter += 1
     cv2.imwrite(new_file_name, img)
+
+def number_to_letter(prediction):
+    alphabet_dic = {0:'A',1:'B',2:'C',3:'D',4:'E',5:'F',6:'G',7:'H',8:'I',9:'J',10:'K',
+    11:'L',12:'M',13:'N',14:'O',15:'P',16:'Q',17:'R',18:'S',19:'T',20:'U',21:'V',22:'W',23:'X'
+    ,24:'Y',25:'Z'}
+    try:        
+        res = alphabet_dic[prediction.item()]
+        return (res)
+    except:
+        print('Error in number_to_letter function')
+        return str(prediction)
